e0bd4931520957ffd85913b838e465c0faa7d4be
eshhusv <ak7527203@gmail.com>
2023-03-03 19:41:43 +0200

    Add project files.

---
+++ twodotthree/Program.cs
@@ -0,0 +1,10 @@
+﻿using twodotthree;
+
+Console.Write("Введите x: ");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите y: ");
+double y = double.Parse(Console.ReadLine());
+
+Solution solution = new Solution(x, y);
+
+Console.WriteLine(solution.isPointLies());
---
+++ twodotthree/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twodotthree
+{
+    internal class Solution
+    {
+        public double x;
+        public double y;
+
+        public Solution(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public string isPointLies()
+        {
+            if (y >= 0 && y <= 2 - x * x) return "Лежит";
+            else return "Не лежит";
+        }
+    }
+}
